8b905cb5e342f560f0127cc8132a71eb56b61871
Shelin <skusmiawati@gmail.com>
2017-04-27 23:32:40 +0700

    Error Analisys Done

---
+++ src/pack/control/controllerToko.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack.control;
+
+import java.util.List;
+import javax.swing.JOptionPane;
+import pack.dao.daoToko;
+import pack.dao.implementToko;
+import pack.model.m_toko;
+import pack.model.tableModelToko;
+import pack.view.home;
+
+/**
+ *
+ * @author Shelin
+ */
+public class controllerToko {
+    home hm;
+    implementToko impToko;
+    List<m_toko> lt;
+    
+     public controllerToko(home hm){
+        this.hm = hm;         
+        impToko = (implementToko) new daoToko();
+        lt = impToko.getAll();
+     }
+ 
+//mengkosongkan isian field
+    public void Reset(){
+    hm.getTxtKode().setText("");
+    hm.getTxtNama().setText("");
+    hm.getTxtHarga().setText("");
+    hm.getCbKategori().setSelectedItem(null);
+    hm.getCbJenis().setSelectedItem(null);      
+    }
+    //menghapus data yang dipilih
+    public void Hapus(){
+    if (!hm.getTxtKode().getText().trim().isEmpty()){
+    }else{
+    String kode = (hm.getTxtKode().getText());
+    impToko.HapusData(kode);
+    JOptionPane.showMessageDialog(hm, "Data berhasil dihapus");
+    }
+    }     
+    //menyimpan data
+    public void SimpanData(){
+    m_toko toko = new m_toko();
+    toko.setkode(hm.getTxtKode().getText());
+    toko.setnama(hm.getTxtNama().getText());
+    toko.setharga(hm.getTxtHarga().getText());
+    toko.setkategori(hm.getCbKategori().getSelectedItem().toString());
+    toko.setjenis(hm.getCbJenis().getSelectedItem().toString());
+    impToko.SimpanData(toko);
+    }
+    //mengubah data
+    public void Ubah(){
+    m_toko toko = new m_toko();
+    toko.setkode(hm.getTxtKode().getText());
+    toko.setnama(hm.getTxtNama().getText());
+    toko.setkategori(hm.getCbKategori().getSelectedItem().toString());
+    toko.setjenis(hm.getCbJenis().getSelectedItem().toString());
+    toko.setharga(hm.getTxtHarga().getText());
+    impToko.UbahData(toko);
+    } 
+    public void isiTable() {
+        lt =impToko.getAll();
+        tableModelToko tmt = new tableModelToko(lt); 
+}
+
+    public void CariKategori() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
